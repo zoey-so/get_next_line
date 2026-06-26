df7322c209022ebc7d7994a031f28acbf852e6ed
zoey-so <xkokaxx@gmail.com>
2026-06-26 21:31:42 +0200

    works fine need to norm and maybe some more tests, project says 50 hours so 10 hours is suspisciusly small ammount

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,28 @@
 /*   By: smilch <smilch@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 15:43:54 by smilch            #+#    #+#             */
-/*   Updated: 2026/06/25 20:45:41 by smilch           ###   ########.fr       */
+/*   Updated: 2026/06/26 20:52:25 by smilch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	init_node(t_rdr **node, int fd, t_rdr **r_head)
+void	init_node(t_rdr *node, int fd, t_rdr **r_head, t_rdr *prev_node)
 {
-	(*node)->fd = fd;
-	(*node)->len = 0;
-	(*node)->pos = 0;
-	(*node)->eof = 0;
-	(*node)->next = *r_head;
+	node->fd = fd;
+	node->len = 0;
+	node->pos = 0;
+	node->eof = 0;
+	if (!*r_head)
+	{
+		node->next = node;
+		*r_head = node;
+	}
+	else
+	{
+		node->next = *r_head;
+		prev_node->next = node;
+	}
 }
 
 t_rdr	*get_rdr(t_rdr **r_head, int fd)
@@ -26,13 +35,12 @@ t_rdr	*get_rdr(t_rdr **r_head, int fd)
 	t_rdr	*new_node;
 	t_rdr	*node;
 
-	write(1, "get\n", 4);
 	node = *r_head;
 	while (node)
 	{
 		if (node->fd == fd)
 			return (node);
-		if(node->next == *r_head)
+		if (node->next == *r_head)
 			break ;
 		node = node->next;
 	}
@@ -45,11 +53,7 @@ t_rdr	*get_rdr(t_rdr **r_head, int fd)
 		free(new_node);
 		return (NULL);
 	}
-	if (!node)
-		*r_head = new_node;
-	else
-		node->next = new_node;
-	init_node(&new_node, fd, r_head);
+	init_node(new_node, fd, r_head, node);
 	return (new_node);
 }
 
@@ -102,25 +106,27 @@ char	*get_from_buf(t_rdr *rdr)
 	return (line);
 }
 
-void	read_to_buf(t_rdr **rdr, t_rdr **r_head)
+t_rdr	*read_to_buf(t_rdr *rdr, t_rdr **r_head)
 {
-	t_rdr	*node;
+	t_rdr	*prev_node;
 
-	node = *rdr;
-	(*rdr)->len = read((*rdr)->fd, (*rdr)->buf, BUFFER_SIZE);
-	write(1, "read\n", 5);
-	if ((*rdr)->len <= 0)
+	prev_node = rdr;
+	rdr->len = read(rdr->fd, rdr->buf, BUFFER_SIZE);
+	if (rdr->len <= 0) // we are in read mode so buffer is empty, if nothing to be read we free the node
 	{
-		while (node->next != *rdr)
-			node = node->next;
-		if (node == *r_head)
+		if (rdr->next == rdr) // means it is the only node in the list so we set head to null
 			*r_head = NULL;
-		node->next = (*rdr)->next;
+		else if (rdr == *r_head)
+			*r_head = rdr->next;
+		while (prev_node->next != rdr) // find the previous node
+			prev_node = prev_node->next;
+		prev_node->next = rdr->next;
 		write(1, "free\n", 5);
-		free((*rdr)->buf);
-		free(*rdr);
-		*rdr = NULL;
+		free(rdr->buf);
+		free(rdr);
+		return (NULL);
 	}
+	return (rdr);
 }
 
 char	*get_next_line(int fd)
@@ -141,11 +147,15 @@ char	*get_next_line(int fd)
 		if (rdr->pos < rdr->len)
 		{
 			line = get_from_buf(rdr);
+			if (concat_line && line && (!rdr || ft_strchr(line, '\n')))
+				return (copy_line(concat_line, line));
 			if (line && (!rdr || ft_strchr(line, '\n')))
 				return (line);
 		}
-		read_to_buf(&rdr, &r_head);
+		rdr = read_to_buf(rdr, &r_head);
 		if (!rdr)
+		{
 			return (copy_line(concat_line, line));
+		}
 	}
 }
